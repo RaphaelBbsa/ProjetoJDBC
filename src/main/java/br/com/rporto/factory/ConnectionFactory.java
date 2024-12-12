66672e05fb2efd4af8064a6b0f175cfb8556ea08
Raphael Barbosa <raphaelbarbosa779@gmail.com>
2024-12-12 14:27:44 -0300

    Primeiro commit com a conexão pro banco sendo criada

--- src/main/java/br/com/rporto/factory/ConnectionFactory.java
+++ src/main/java/br/com/rporto/factory/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
+a
 public class ConnectionFactory {
     private static String url;
     private static String username;
